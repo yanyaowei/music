7812ceb60f6462c591663e809381793c27597c8f
yanyaowei <2441771478@qq.com>
2020-10-31 12:22:00 +0800

    Site updated: 2020-10-31 12:21:59

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 /*
   1:歌曲搜索接口
-    请求地址:https://autumnfish.cn/search
+    请求地址:https://api.imjad.cn/cloudmusic/search
     请求方法:get
     请求参数:keywords(查询关键字)
     响应内容:歌曲搜索结果
 
   2:歌曲url获取接口
-    请求地址:https://autumnfish.cn/song/url
+    请求地址:https://api.imjad.cn/cloudmusic/url
     请求方法:get
     请求参数:id(歌曲id)
     响应内容:歌曲url地址
   3.歌曲详情获取
-    请求地址:https://autumnfish.cn/song/detail
+    请求地址:https://api.imjad.cn/cloudmusic/detail
     请求方法:get
     请求参数:ids(歌曲id)
     响应内容:歌曲详情(包括封面信息)
   4.热门评论获取
-    请求地址:https://autumnfish.cn/comment/hot?type=0
+    请求地址:https://api.imjad.cn/cloudmusic/comment/hot?type=0
     请求方法:get
     请求参数:id(歌曲id,地址中的type固定为0)
     响应内容:歌曲的热门评论
   5.mv地址获取
-    请求地址:https://autumnfish.cn/mv/url
+    请求地址:https://api.imjad.cn/cloudmusic/mv/url
     请求方法:get
     请求参数:id(mvid,为0表示没有mv)
     响应内容:mv的地址
@@ -50,7 +50,7 @@ var app = new Vue({
     // 歌曲搜索
     searchMusic: function() {
       var that = this;
-      axios.get("http://msearchcdn.kugou.com/api/v3/search/song?showtype=14&highlight=em&pagesize=30&tag_aggr=1&tagtype=%E5%85%A8%E9%83%A8&plat=0&sver=5&keyword=%E4%BD%A0%E5%A5%BD&correct=1&api_ver=1&version=9108&page=1&area_code=1&tag=1&with_res_tag=1?keywords=" + this.query).then(
+      axios.get("https://api.imjad.cn/cloudmusic/?type=search&keywords=" + this.query).then(
         function(response) {
           // console.log(response);
           that.musicList = response.data.result.songs;
@@ -64,7 +64,7 @@ var app = new Vue({
       //   console.log(musicId);
       var that = this;
       // 获取歌曲地址
-      axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
+      axios.get("https://api.imjad.cn/cloudmusic/song/url?id=" + musicId).then(
         function(response) {
           // console.log(response);
           // console.log(response.data.data[0].url);
@@ -74,7 +74,7 @@ var app = new Vue({
       );
 
       // 歌曲详情获取
-      axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
+      axios.get("https://api.imjad.cn/cloudmusic/song/detail?ids=" + musicId).then(
         function(response) {
           // console.log(response);
           // console.log(response.data.songs[0].al.picUrl);
@@ -84,7 +84,7 @@ var app = new Vue({
       );
 
       // 歌曲评论获取
-      axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
+      axios.get("https://api.imjad.cn/cloudmusic/comment/hot?type=0&id=" + musicId).then(
         function(response) {
           // console.log(response);
           // console.log(response.data.hotComments);
@@ -106,7 +106,7 @@ var app = new Vue({
     // 播放mv
     playMV: function(mvid) {
       var that = this;
-      axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
+      axios.get("https://api.imjad.cn/cloudmusic/mv/url?id=" + mvid).then(
         function(response) {
           // console.log(response);
           console.log(response.data.data.url);
